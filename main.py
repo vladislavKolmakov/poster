b577d1904828889d31f354e763fd256418927349
vladislavKolmakov <52298225+vladislavKolmakov@users.noreply.github.com>
2023-03-01 19:59:43 +0300

    added endpoint to create note

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from typing import List, Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from enum import Enum
 
 app = FastAPI(
     title='Poster'
@@ -9,12 +10,12 @@ app = FastAPI(
 #################################
 
 fake_users = [
-    {'id': 0, 'name': 'Anton', 'surname': 'Belyaev', 'age': 23, 'nickname': 'ANTONIDAS', 'sex': 'male'},
-    {'id': 1, 'name': 'Vladislav', 'surname': 'Kolmakov', 'age': 24, 'nickname': 'imfoslash', 'sex': 'male'},
-    {'id': 2, 'name': 'Sonya', 'surname': 'Chukanova', 'age': 27, 'nickname': 'kotik', 'sex': 'female'},
+    {'id': 0, 'name': 'Anton', 'surname': 'Belyaev', 'age': 23, 'nickname': 'ANTONIDAS', 'gender': 'male'},
+    {'id': 1, 'name': 'Vladislav', 'surname': 'Kolmakov', 'age': 24, 'nickname': 'imfoslash', 'gender': 'male'},
+    {'id': 2, 'name': 'Sonya', 'surname': 'Chukanova', 'age': 27, 'nickname': 'kotik', 'gender': 'female'},
 ]
 
-fake_posts = [
+fake_notes = [
     {'id': 0, 'uid': 1, 'content': 'встал, покушал, в целом не плохо'},
     {'id': 1, 'uid': 0, 'content': 'я верховный архимаг АНТОНИДОС!!!'},
     {'id': 2, 'uid': 2, 'content': 'мяу мяу мяу'},
@@ -23,13 +24,35 @@ fake_posts = [
 #################################
 
 
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+    nickname: str = Field(max_length=20)
+    age: int = Field(ge=0)
+    gender: str
+
+class Note(BaseModel):
+    id: int
+    uid: int
+    content: str
+
+#################################
+
+
 # TODO fing all posts by user.name/user.surname/user.nickname/date
-@app.get('/get_post/{post_id}')
-def hello(post_id: int):
-    return [post for post in fake_posts  if post.get('id') == post_id]
+@app.get('/get_notes/{notes_id}', response_model=List[Note])
+def get_notes(notes_id: int):
+    return [notes for notes in fake_notes  if notes.get('id') == notes_id]
 
 # TODO find user by user.name/user.surname/user.nickname
 @app.get('/get_user/{user_id}')
 def get_user(user_id: int):
     current_user = list(filter(lambda user: user.get("id") == user_id, fake_users))[0]
-    return {"status": 200, "data": current_user}
+    return current_user
+
+
+@app.post('/create_note')
+def add_post(note: List[Note]):
+    fake_notes.extend(note)
+    return {'status': 200, 'data': fake_notes}
